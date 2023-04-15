fbc4acd2561594c3b62248cd3223dd175601c694
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-15 22:58:01 +0700

    feat(api): test get request from client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <div> Hello</div>;
-}
+const API_ENDPOINT =
+  "https://m0rz74a4y4.execute-api.ap-southeast-1.amazonaws.com/dev/movies";
+
+const Home = () => {
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(API_ENDPOINT);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log("Here");
+        console.error(error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  return <div> Hello </div>;
+};
+
+export default Home;
