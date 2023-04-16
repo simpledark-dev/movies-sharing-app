95cf27414bee3aab207c2e3e4530fb4fa962b3f1
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-17 05:15:27 +0700

    feat(movies): update and retrieve movies, movies_sharing table

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,13 +1,17 @@
 const db = require("../config/db");
 
-// Get all movies
-exports.getAllMovies = (req, res) => {
-  const query = "SELECT * FROM movies";
+// Get all shared movies
+exports.getAllSharedMovies = (req, res) => {
+  const query = `
+    SELECT m.*, ms.user_id FROM movies m
+    JOIN movies_sharing ms ON m.id = ms.movie_id
+    JOIN users u ON ms.user_id = u.id
+  `;
 
   db.query(query, (err, results) => {
     if (err) {
       console.log(err);
-      return res.status(500).json({ error: "Error getting movies" });
+      return res.status(500).json({ error: "Error getting shared movies" });
     }
 
     return res.json(results);
@@ -18,6 +22,7 @@ exports.getAllMovies = (req, res) => {
 exports.postMovie = (req, res) => {
   const { title, description, youtubeVideoId } = req.body;
 
+  // Add movie to movies table
   const query =
     "INSERT INTO movies (title, description, youtube_video_id) VALUES (?, ?, ?)";
   const values = [title, description, youtubeVideoId];
@@ -28,9 +33,28 @@ exports.postMovie = (req, res) => {
       return res.status(500).json({ error: "Error adding movie" });
     }
 
+    // Update the movies_sharing table
+    const error = updateMoviesSharingTable(req.body.user.id, results.insertId);
+
+    if (error) {
+      return res.status(500).json({ error: "Error updating movies sharing" });
+    }
+
     return res.json({
       message: "Movie added successfully",
       id: results.insertId,
     });
+  });
+};
+
+const updateMoviesSharingTable = (userId, movieId) => {
+  const query = "INSERT INTO movies_sharing (user_id, movie_id) VALUES (?, ?)";
+  const values = [userId, movieId];
+
+  db.query(query, values, (err) => {
+    if (err) {
+      console.log(err);
+      return err;
+    }
   });
 };
--- server/routes/movie.js
+++ server/routes/movie.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
 
-router.get("/", movieController.getAllMovies);
+router.get("/", movieController.getAllSharedMovies);
 router.post("/", movieController.postMovie);
 
 module.exports = router;
