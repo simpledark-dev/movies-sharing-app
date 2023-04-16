3739b1c09e165b4cca34d8b195a0e8ab9ffb699c
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-17 04:14:55 +0700

    feat(api): implement movie controllers

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,7 +1,36 @@
+const db = require("../config/db");
+
+// Get all movies
 exports.getAllMovies = (req, res) => {
-  res.send("Movies");
+  const query = "SELECT * FROM movies";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Error getting movies" });
+    }
+
+    return res.json(results);
+  });
 };
 
+// Add a new movie
 exports.postMovie = (req, res) => {
-  res.send(null);
+  const { title, description, youtubeVideoId } = req.body;
+
+  const query =
+    "INSERT INTO movies (title, description, youtube_video_id) VALUES (?, ?, ?)";
+  const values = [title, description, youtubeVideoId];
+
+  db.query(query, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Error adding movie" });
+    }
+
+    return res.json({
+      message: "Movie added successfully",
+      id: results.insertId,
+    });
+  });
 };
