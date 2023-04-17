0cb0fdc861bec3242279f490ed0c55df5c9ed5c5
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-18 02:19:00 +0700

    feat(ui-logic): create global user state in react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
+import UserContext from "@/context/userContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
