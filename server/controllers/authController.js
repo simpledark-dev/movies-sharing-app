21e0edd512c10fa34b76f3598ddb7842a68ce140
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-18 00:56:19 +0700

    feat(auth): implement register and login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,80 @@
-exports.register = (req, res) => {
-  res.send("register");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
+
+// Function to generate JWT token
+const generateToken = (user) => {
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  const secret = "mysecretkey";
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
 };
 
+// Login controller
 exports.login = (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  const query = "SELECT * FROM users WHERE email = ?";
+  const values = [email];
+
+  db.query(query, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Error logging in" });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const user = results[0];
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Error logging in" });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+
+      const token = generateToken(user);
+      return res.json({ token });
+    });
+  });
+};
+
+// Register controller
+exports.register = (req, res) => {
+  const { email, password } = req.body;
+
+  // Hash password
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Error registering user" });
+    }
+
+    const query = "INSERT INTO users (email, password) VALUES (?, ?)";
+    const values = [email, hashedPassword];
+
+    db.query(query, values, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Error registering user" });
+      }
+
+      const user = {
+        id: results.insertId,
+        email: email,
+      };
+      const token = generateToken(user);
+      return res.json({ token });
+    });
+  });
 };
