4f41e958b8696250fbed39412dab5f40d49e5b29
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-18 16:04:25 +0700

    feat(loading): display loading spinner while retrieving movies from db

--- client/components/SeeMoviesSection.tsx
+++ client/components/SeeMoviesSection.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { BASE_API_URL } from "@/config/constants";
 import Container from "@/layout/container";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const SeeMoviesSection = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(null);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -11,7 +12,10 @@ const SeeMoviesSection = () => {
         const response = await fetch(`${BASE_API_URL}/movies`);
         const data = await response.json();
         console.log(data);
-        setMovies(data);
+        console.log();
+        if (response.status === 200) {
+          setMovies(data);
+        }
       } catch (error) {
         console.error(error);
       }
@@ -22,26 +26,32 @@ const SeeMoviesSection = () => {
   return (
     <Container>
       <div className="  space-y-8">
-        {movies.map((movie: any) => {
-          return (
-            <div key={movie.id} className="flex gap-8 justify-center">
-              <iframe
-                className="w-1/3 aspect-video max-h-56 rounded"
-                src={`https://www.youtube.com/embed/${movie.youtube_video_id}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-              />
-              <div className="w-1/2">
-                <h3 className="text-lg text-red-500">{movie.title}</h3>
-                <p>Shared by: {movie.email}</p>
-                <h3 className="text-md">Description:</h3>
-                <p className="text-sm">{movie.description}</p>
+        {movies ? (
+          movies.map((movie: any) => {
+            return (
+              <div key={movie.id} className="flex gap-8 justify-center">
+                <iframe
+                  className="w-1/3 aspect-video max-h-56 rounded"
+                  src={`https://www.youtube.com/embed/${movie.youtube_video_id}`}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+                <div className="w-1/2">
+                  <h3 className="text-lg text-red-500">{movie.title}</h3>
+                  <p>Shared by: {movie.email}</p>
+                  <h3 className="text-md">Description:</h3>
+                  <p className="text-sm">{movie.description}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="flex justify-center mt-[30vh]">
+            <ClipLoader />
+          </div>
+        )}
       </div>
     </Container>
   );
