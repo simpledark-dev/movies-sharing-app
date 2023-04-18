8544daed450ef890eae7a8aa72162661b3137c7b
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-18 20:15:34 +0700

    fix(api): use deployed api endpoint

--- client/config/constants.ts
+++ client/config/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "http://localhost:5000";
-export const YOUTUBE_API_KEY = "<api-key>";
+export const BASE_API_URL = "https://movies-sharing-app-backend.vercel.app";
+export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
