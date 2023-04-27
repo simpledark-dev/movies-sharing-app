91b078ebc4695126182f924e8d264cdc36e323e7
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-28 00:32:42 +0700

    fix: display error login messages

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -7,6 +7,7 @@ const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [actionType, setActionType] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLoginRegister = async (e: any) => {
     e.preventDefault();
@@ -24,6 +25,7 @@ const LoginForm = () => {
         setUser(data.user);
         document.cookie = `jwtToken=${data.token}`;
       } catch (error) {
+        setErrorMessage("Login Error!");
         console.error(error);
       }
     } else if (actionType === "register") {
@@ -39,48 +41,52 @@ const LoginForm = () => {
         setUser(data.user);
         document.cookie = `jwtToken=${data.token}`;
       } catch (error) {
+        setErrorMessage("Register Error!");
         console.error(error);
       }
     }
   };
 
   return (
-    <form onSubmit={handleLoginRegister} className="flex gap-2 items-center">
-      <label className=" space-x-2">
-        <span>Email:</span>
-        <input
-          className="shadow border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          required
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label className=" space-x-2">
-        <span>Password:</span>
-        <input
-          className="shadow border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          required
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
-        type="submit"
-        onClick={() => setActionType("login")}
-      >
-        Login
-      </button>
-      <button
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
-        type="submit"
-        onClick={() => setActionType("register")}
-      >
-        Register
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleLoginRegister} className="flex gap-2 items-center">
+        <label className=" space-x-2">
+          <span>Email:</span>
+          <input
+            className="shadow border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className=" space-x-2">
+          <span>Password:</span>
+          <input
+            className="shadow border rounded py-1 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
+          type="submit"
+          onClick={() => setActionType("login")}
+        >
+          Login
+        </button>
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-4 border border-gray-400 rounded shadow"
+          type="submit"
+          onClick={() => setActionType("register")}
+        >
+          Register
+        </button>
+      </form>
+      <p className="text-red-400 text-sm">{errorMessage}</p>
+    </>
   );
 };
 
